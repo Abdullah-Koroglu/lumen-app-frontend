54f2513434164bcd84387f78557a4fad733ff340
Abdullah-Koroglu <abdullah34koroglu@gmail.com>
2022-04-03 16:01:05 +0300

    users page

--- src/admin/users/List.jsx
+++ src/admin/users/List.jsx
@@ -5,27 +5,28 @@ import { accountService } from '@/_services';
 
 function List({ match }) {
     const { path } = match;
+    const currentUser = accountService.userValue;
     const [users, setUsers] = useState(null);
 
     useEffect(() => {
         accountService.getAll().then(x => setUsers(x));
     }, []);
 
-    function deleteUser(id) {
-        setUsers(users.map(x => {
-            if (x.id === id) { x.isDeleting = true; }
-            return x;
-        }));
-        accountService.delete(id).then(() => {
-            setUsers(users => users.filter(x => x.id !== id));
-        });
-    }
+    // function deleteUser(id) {
+    //     setUsers(users.map(x => {
+    //         if (x.id === id) { x.isDeleting = true; }
+    //         return x;
+    //     }));
+    //     accountService.delete(id).then(() => {
+    //         setUsers(users => users.filter(x => x.id !== id));
+    //     });
+    // }
 
     return (
         <div>
-            <h1>Users</h1>
+            <h2>Users</h2>
             <p>All users from secure (admin only) api end point:</p>
-            <Link to={`${path}/add`} className="btn btn-sm btn-success mb-2">Add User</Link>
+            {/* <Link to={`${path}/add`} className="btn btn-sm btn-success mb-2">Add User</Link> */}
             <table className="table table-striped">
                 <thead>
                     <tr>
@@ -37,18 +38,20 @@ function List({ match }) {
                 </thead>
                 <tbody>
                     {users && users.map(user =>
-                        <tr key={user.id}>
+                        <tr 
+                            style={currentUser.id == user.id ? { backgroundColor: 'red'} : null}
+                        key={user.id}>
                             <td>{user.firstName} {user.lastName}</td>
                             <td>{user.email}</td>
                             <td>{user.role}</td>
-                            <td style={{ whiteSpace: 'nowrap' }}>
-                                <Link to={`${path}/edit/${user.id}`} className="btn btn-sm btn-primary mr-1">Edit</Link>
+                            <td>
+                                {/* <Link to={`${path}/edit/${user.id}`} className="btn btn-sm btn-primary mr-1">Edit</Link>
                                 <button onClick={() => deleteUser(user.id)} className="btn btn-sm btn-danger" style={{ width: '60px' }} disabled={user.isDeleting}>
                                     {user.isDeleting 
                                         ? <span className="spinner-border spinner-border-sm"></span>
                                         : <span>Delete</span>
                                     }
-                                </button>
+                                </button> */}
                             </td>
                         </tr>
                     )}
